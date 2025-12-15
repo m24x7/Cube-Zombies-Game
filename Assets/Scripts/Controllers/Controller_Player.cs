c2c6284758fdbac6fb54043b4af5ba49e60773bf
m24x7 <59781877+m24x7@users.noreply.github.com>
2025-12-15 12:32:07 -0700

    Temp disabled jumping

--- Assets/Scripts/Controllers/Controller_Player.cs
+++ Assets/Scripts/Controllers/Controller_Player.cs
@@ -155,8 +155,8 @@ public class Controller_Player : Parent_Entity
 
     private void MoveUpdate()
     {
-        JumpAndGravity();
-        GroundedCheck();
+        //JumpAndGravity();
+        //GroundedCheck();
         Move();
     }
 
